015cb49ee19e0a266c3c6db385a05ae49e38534d
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-09-26 08:30:29 -0600

    divide trips into categories for the display

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,7 @@ import { fetchData, postData } from './api-calls.js';
 import Traveler from './Traveler.js';
 
 
-const tripCardsContainer = document.querySelector('#tripCardsContainer')
+const pendingTripCards = document.querySelector('#pendingTripCardsContainer')
 const yearsExpense = document.querySelector('#expenses')
 const name = document.querySelector('#nameOfUser')
 const addDestinations = document.querySelector('#newTripDestination')
@@ -20,6 +20,8 @@ const travelerCount = document.getElementById('travelerNumber')
 const submitNewTrip = document.getElementById('submitNewTrip')
 const newTripForm = document.getElementById('newTripForm')
 const tripEstimate = document.getElementById('estimateValue')
+const pastTripCards = document.getElementById('pastTripCardsContainer')
+const upcomingTripCards = document.getElementById('UpcomingTripCardsContainer')
 
 
 
@@ -34,12 +36,16 @@ function fetchAllData() {
     travelerData = data[0],
     tripData = data[1],
     destinationData = data[2]
+    console.log('1', tripData)
    
     currentUser = new Traveler(travelerData.travelers[Math.floor(Math.random() * travelerData.travelers.length)], tripData)
+    console.log('1', currentUser)
     
 
     loadUsername()
-    loadUserTrips()
+    loadPendingUserTrips()
+    loadPastUserTrips()
+    loadUpcomingUserTrips()
     loadYearsExpense()
     addDestinationOptions()
   })
@@ -56,11 +62,11 @@ function loadUsername() {
 }
 
 
-function loadUserTrips() {
+function loadPendingUserTrips() {
 currentUser.allUserTrips.forEach((trip)=> {
   destinationData.destinations.forEach((destination) =>{
-    if (destination.id === trip.destinationID) {
-  tripCardsContainer.innerHTML += `<article class="trip-card">
+    if (destination.id === trip.destinationID && trip.status === 'pending') {
+  pendingTripCards.innerHTML += `<article class="trip-card">
   <img class="card-images" src=${destination.image} alt=${destination.alt} height="240px" width="350px">
   <p class="destination">destination: ${destination.destination}</p>
   <p class="date">date: ${trip.date}</p>
@@ -68,10 +74,44 @@ currentUser.allUserTrips.forEach((trip)=> {
   <p class="traveler-count">Travelers: ${trip.travelers}</p>
   <p class="trip-status">Status: ${trip.status}</p>
 </article>`
-    }
+} 
 })
 })
 }
+function loadPastUserTrips() {
+  const todayDate = new Date().toISOString().slice(0, 10).split("-").join("/");
+  currentUser.allUserTrips.forEach((trip)=> {
+    destinationData.destinations.forEach((destination) =>{
+      if (destination.id === trip.destinationID && trip.date < todayDate && trip.status === 'approved') {
+    pastTripCards.innerHTML += `<article class="trip-card">
+    <img class="card-images" src=${destination.image} alt=${destination.alt} height="240px" width="350px">
+    <p class="destination">destination: ${destination.destination}</p>
+    <p class="date">date: ${trip.date}</p>
+    <p class="duration">duration: ${trip.duration}</p>
+    <p class="traveler-count">Travelers: ${trip.travelers}</p>
+    <p class="trip-status">Status: ${trip.status}</p>
+  </article>`
+      }
+  })
+  })
+  }
+  function loadUpcomingUserTrips() {
+    const todayDate = new Date().toISOString().slice(0, 10).split("-").join("/");
+    currentUser.allUserTrips.forEach((trip)=> {
+      destinationData.destinations.forEach((destination) =>{
+        if (destination.id === trip.destinationID && trip.date > todayDate ) {
+      upcomingTripCards.innerHTML += `<article class="trip-card">
+      <img class="card-images" src=${destination.image} alt=${destination.alt} height="240px" width="350px">
+      <p class="destination">destination: ${destination.destination}</p>
+      <p class="date">date: ${trip.date}</p>
+      <p class="duration">duration: ${trip.duration}</p>
+      <p class="traveler-count">Travelers: ${trip.travelers}</p>
+      <p class="trip-status">Status: ${trip.status}</p>
+    </article>`
+        }
+    })
+    })
+    }
 
 function loadYearsExpense() {
   let sum = 0
@@ -110,10 +150,10 @@ newTripForm.addEventListener('submit', (event) => {
     status: 'pending',
     suggestedActivities: []
   }
-
   if (!newTripData.date.includes('/')) {
     alert('Please enter date in the correct format')
   } else {
+
     postData('http://localhost:3001/api/v1/trips', newTripData) 
     event.target.reset()
   }
@@ -123,15 +163,27 @@ newTripForm.addEventListener('submit', (event) => {
 
 function fetchNewData() {
   tripCardsContainer.innerHTML = ''
+  pastTripCards.innerHTML = ''
+  upcomingTripCards.innerHTML = ''
   Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')])
   .then((data) => {
     travelerData = data[0],
     tripData = data[1],
     destinationData = data[2]
+    console.log(tripData)
+    console.log('2', currentUser.allUserTrips)
+
+    tripCardsContainer.innerHTML = ''
+    pastTripCards.innerHTML = ''
+    upcomingTripCards.innerHTML = ''
+    loadPendingUserTrips()
+    loadPastUserTrips()
+    loadUpcomingUserTrips()
+
 
 })
 }
-newTripForm.addEventListener('change', (event) => {
+newTripForm.addEventListener('change', () => {
   let sum = 0
   destinationData.destinations.forEach((destination) => {
     if (destination.destination === addDestinations.value) {
